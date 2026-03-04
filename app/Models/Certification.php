f1bfe21e4bcee2bb86cce99224ca80e64f358173
shofialafarah <shofia.lafarah74@gmail.com>
2026-03-04 08:29:45 +0700

    Update Models certification

--- app/Models/Certification.php
+++ app/Models/Certification.php
@@ -13,4 +13,18 @@ class Certification extends Model
         'image',
         'is_active'
     ];
+
+    // Menambah casting agar Laravel membacanya sebagai boolean
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    /**
+     * Memaksa nilai menjadi boolean murni atau string 'true'/'false' 
+     * agar PostgreSQL Supabase tidak bingung.
+     */
+    public function setIsActiveAttribute($value)
+    {
+        $this->attributes['is_active'] = filter_var($value, FILTER_VALIDATE_BOOLEAN) ? 'true' : 'false';
+    }
 }
